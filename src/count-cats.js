3a97205a7066cf165c25e948f1ed27ca250cd183
Maxim <maxim.bma10@gmail.com>
2021-09-19 01:26:48 +0300

    fix: count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,14 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function countCats(matrix) {
+  let count = 0;
+  for (let row of matrix) {
+    for (let col of row) {
+      if (col === '^^') {
+        count++
+      }
+    }
+  }
+  return count
 }
